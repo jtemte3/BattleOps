c7e4528cf89e064f4a80965debf783f57c609c56
jtemte3 <47343054+jtemte3@users.noreply.github.com>
2026-01-26 19:03:50 -0600

    Updating the HUDToggle script to hide meshes while still showing shadows when the hud is turned off

--- Assets/Scripts/UI/HUDToggle.cs
+++ Assets/Scripts/UI/HUDToggle.cs
@@ -5,7 +5,8 @@ public class HUDToggle : MonoBehaviour
 {
     public ControlSchemeManager controlScheme;
     public bool showHUD;
-    public List<GameObject> gameObjects = new List<GameObject>();
+    public List<GameObject> gameObjectstoDisable = new List<GameObject>();
+    public List<GameObject> gameObjectstoHide = new List<GameObject>();
 
     // Update is called once per frame
     void Update()
@@ -27,9 +28,40 @@ public class HUDToggle : MonoBehaviour
 
     void SetHUDElements(bool state)
     {
-        foreach (GameObject element in gameObjects)
+        if (gameObjectstoDisable.Count > 0)
         {
-            element.SetActive(state);
+            foreach (GameObject element in gameObjectstoDisable)
+            {
+                element.SetActive(state);
+            }
+        }
+        if (gameObjectstoHide.Count > 0)
+        {
+            foreach (GameObject element in gameObjectstoHide)
+            {
+                if (state == false)
+                {
+                    if (element.GetComponent<SkinnedMeshRenderer>() != null)
+                    {
+                        element.GetComponent<SkinnedMeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+                    }
+                    else
+                    {
+                        element.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+                    }
+                }
+                else
+                {
+                    if (element.GetComponent<SkinnedMeshRenderer>() != null)
+                    {
+                        element.GetComponent<SkinnedMeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                    }
+                    else
+                    {
+                        element.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                    }
+                }
+            }
         }
     }
 }
