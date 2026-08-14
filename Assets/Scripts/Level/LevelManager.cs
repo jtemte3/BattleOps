6eaea9c6556de52e9b9a1662f3ee47ccb5077e33
jtemte3 <47343054+jtemte3@users.noreply.github.com>
2026-08-14 04:21:24 -0500

    Level Manager Modernization - Updated the Level Manager to match the current mission implementation - Updated the Loading bar to reference the Level Manager for loading info - Modernised the event calls when the scene starts and when the scene is loaded to utilize UnityEvents - Set the Canvas UI elements to be loaded correctly on start with an OnStart UnityEvent

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -1,90 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LevelManager : MonoBehaviour
 {
     public MissionSetupHandler missionSetupHandler;
-    public GridVoronoiCity cityParent;
-    public MaskedPerlinTerrain terrainObject;
-    public HelicopterSequence infilVehicle;
-    public GameObject LoadingScreen;
-    public AudioSource musicPlayer;
+    public bool hasLoaded = true;
+    public float loadingPercentage = 100f;
 
-    public GameObject playerController;
-    public GameObject objectivePrefab;
-    //public MissionManager missionManager;
-    public bool landInCity = false;
-    public bool reGenerateTerrain = true;
-
-    public float timer;
+    public float loadScreenHoldTimer = 3f;
     float startTime;
     float endTime;
     bool isTimerSet = false;
+    public HelicopterSequence infilVehicle;
+
+
+    [Tooltip("Events to trigger on event completion")]
+    public UnityEvent OnStart;
+    public UnityEvent onStart => OnStart;
+
+    [Tooltip("Events to trigger on event completion")]
+    public UnityEvent OnLoadFinish;
+    public UnityEvent onLoadFinish => OnLoadFinish;
+
+    
 
     private bool hasStarted = false;
 
     private void Start()
     {
-        if (reGenerateTerrain)
-        {
-            terrainObject.SetRandomNoiseOffset();
-            terrainObject.Generate();
-        }
 
         // Trigger mission setup after terrain is ready
         if (missionSetupHandler != null)
         {
             missionSetupHandler.SetupMission();
         }
+
+        onStart.Invoke();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (cityParent.loadingPercentage.Equals(1))
+        if (hasLoaded && isTimerSet.Equals(false))
         {
-            if (isTimerSet.Equals(false))
+            startTime = Time.time;
+            endTime = startTime + loadScreenHoldTimer;
+            isTimerSet = true;
+
+            List<GameObject> sequence = infilVehicle.RoutePositions[infilVehicle.routId].infilApproachRoute;
+
+            GameObject endObj = sequence[sequence.Count - 1];
+            GameObject hoverObj = sequence[sequence.Count - 2];
+        }
+
+        if (isTimerSet && Time.time >= endTime)
+        {
+            if (hasStarted == false)
             {
-                startTime = Time.time;
-                endTime = startTime + timer;
-                isTimerSet = true;
+                hasStarted = true;
 
-                List<GameObject> sequence = infilVehicle.RoutePositions[infilVehicle.routId].infilApproachRoute;
-
-                GameObject endObj = sequence[sequence.Count - 1];
-                GameObject hoverObj = sequence[sequence.Count - 2];
-
-                if (landInCity)
-                {
-                    (Vector3 landingPos, Vector3 directionPos) = cityParent.GetLandingZoneRoadGen();
-
-                    Vector3 hoverPos = landingPos;
-                    hoverPos.y += 30.1f;
-
-                    endObj.transform.position = landingPos;
-                    hoverObj.transform.position = hoverPos;
-
-                    endObj.transform.LookAt(directionPos);
-                }
-            }
-
-            if (Time.time >= endTime)
-            {
-                if (hasStarted == false)
-                {
-                    hasStarted = true;
-                    playerController.SetActive(true);
-                    LoadingScreen.SetActive(false);
-                    infilVehicle.beginFlying = true;
-                    infilVehicle.StartAudio();
-                    musicPlayer.Play();
-
-                    /*if (missionManager)
-                    {
-                        missionManager.isMissionLoaded = true;
-                    }*/
-                }
+                OnLoadFinish.Invoke();
             }
         }
     }
